83bfef18851652e41b2ae618283e04d9b8820049
firewings2515 <capricorn9908@gmail.com>
2021-01-03 17:07:27 +0800

    add  AOV

---
+++ Ambient Occlusion and Contour/Assets/AOV/AOVCameraScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class AOVCameraScript : MonoBehaviour
+{
+    private RenderTexture bufferAOV;
+    private Material matRestoreDepth;
+    private Material matVolumes;
+    private Material matFinal;
+    private CameraEvent cmdsEvent = CameraEvent.BeforeImageEffectsOpaque;
+    private CommandBuffer cmdsAOV;
+
+    public float maxObscuranceDistance = 0.5f;
+    public float falloffExponent = 1.0f;
+    public bool debugShow = false;
+    public float debugMix = 1.0f;
+
+    void OnEnable()
+    {
+        Debug.Log("Enable");
+        var camera = GetComponent<Camera>();
+        camera.depthTextureMode = DepthTextureMode.DepthNormals;
+
+        bufferAOV = new RenderTexture(camera.pixelWidth, camera.pixelHeight, 24, RenderTextureFormat.RHalf);
+
+        matRestoreDepth = (Material)Resources.Load("AOVRestoreDepth", typeof(Material));
+        matVolumes = (Material)Resources.Load("AOVVolumes", typeof(Material));
+        matFinal = (Material)Resources.Load("AOVFinal", typeof(Material));
+
+        cmdsAOV = new CommandBuffer();
+        cmdsAOV.name = "AOV";
+
+        camera.RemoveAllCommandBuffers();
+        camera.AddCommandBuffer(cmdsEvent, cmdsAOV);
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Disable");
+        var camera = GetComponent<Camera>();
+        camera.RemoveCommandBuffer(cmdsEvent, cmdsAOV);
+    }
+
+    void LateUpdate()
+    {
+        cmdsAOV.Clear();
+        RenderAOV(cmdsAOV);
+    }
+
+    void RenderAOV(CommandBuffer cmd)
+    {
+        var camera = GetComponent<Camera>();
+
+        cmd.Blit((Texture)null, bufferAOV, matRestoreDepth);
+        cmd.SetRenderTarget(bufferAOV);
+        cmd.ClearRenderTarget(true, true, new Color32(0, 0, 0, 255));
+        cmd.SetGlobalFloat("AOV_maxObscuranceDistance", maxObscuranceDistance);
+        cmd.SetGlobalFloat("AOV_falloffExponent", falloffExponent);
+        cmd.SetGlobalMatrix("AOV_inverseView", camera.cameraToWorldMatrix);
+
+        var aovObjects = GameObject.FindGameObjectsWithTag("AOV");
+        foreach (var aovObject in aovObjects)
+        {
+            var meshFilters = aovObject.GetComponentsInChildren<MeshFilter>();
+            foreach (var meshFilter in meshFilters)
+            {
+                var mesh = meshFilter.sharedMesh;
+                for (int submeshIndex = 0; submeshIndex < mesh.subMeshCount; submeshIndex++)
+                {
+                    cmd.DrawMesh(meshFilter.sharedMesh, meshFilter.transform.localToWorldMatrix, matVolumes, submeshIndex);
+                }
+            }
+        }
+
+        cmd.SetRenderTarget((RenderTexture)null);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        matFinal.SetTexture("_AccessibilityTex", bufferAOV);
+        matFinal.SetFloat("_DebugMix", debugMix);
+        Graphics.Blit(source, dest, matFinal);
+    }
+
+    void OnGUI()
+    {
+        if (debugShow)
+        {
+            GUI.DrawTexture(new Rect(0, 0, bufferAOV.width / 4, bufferAOV.height / 4), bufferAOV, ScaleMode.ScaleToFit, true);
+        }
+    }
+}
---
+++ Ambient Occlusion and Contour/Assets/AOV/AOVFinal.shader
@@ -0,0 +1,55 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Hidden/AOVFinal"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_AccessibilityTex ("Texture", 2D) = "white" {}
+		_DebugMix ("Debug Mix", Range (0,1)) = 1
+	}
+	SubShader
+	{
+		Pass
+		{
+			ZTest Always
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+		
+			sampler2D _MainTex;
+			uniform float2 _MainTex_TexelSize;
+			sampler2D _AccessibilityTex;
+			float _DebugMix;
+
+			struct v2f {
+				float4 vertex : SV_POSITION;
+				float2 uvMain : TEXCOORD0;
+				float2 uvAOV : TEXCOORD1;
+			};
+
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uvMain = v.texcoord.xy;
+				o.uvAOV = v.texcoord.xy;
+				#if UNITY_UV_STARTS_AT_TOP
+				if (_MainTex_TexelSize.y < 0)
+					o.uvAOV.y = 1 - o.uvAOV.y;
+				#endif
+				return o;
+			}
+ 
+			fixed4 frag(v2f i) : COLOR
+			{
+				fixed4 base = tex2D(_MainTex, i.uvMain);
+				fixed4 ao = tex2D(_AccessibilityTex, i.uvAOV).xxxx;
+				return lerp(float4(1,1,1,1), base, _DebugMix) * (1 - ao);
+			}
+			ENDCG
+		}
+	}
+}
